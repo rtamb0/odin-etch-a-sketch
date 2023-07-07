c2da3054177cecb8e38ac90d365d26b576340502
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-07-07 17:09:51 +0700

    Create psuedocode for making divs

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const divsContainer = document.querySelector('#square-divs');
 const div = document.createElement('div');
 
 // Create divs 16 times with loop
+
+// FOR each loop
+    // Create div element
+    // Append it to divsContainer
+    // Repeat 16 times
+// END FOR
